527b11868f6af34175d036dfc7faf9ef5abca5b5
Austin Gebauer <agebauer@hashicorp.com>
2024-08-06 08:08:14 -0700

    move_zeroes_283: solved 2

--- move_zeroes_283/solution.go
+++ move_zeroes_283/solution.go
@@ -1,9 +1,9 @@
 package move_zeroes_283
 
-// Note: tricky problem!
 // Ex: [1,0,0,3,12]
-//            i
-//        p
+//
+//	    i
+//	p
 func moveZeroes(nums []int) {
 	placement := 0
 	for i := 0; i < len(nums); i++ {
@@ -13,5 +13,19 @@ func moveZeroes(nums []int) {
 			nums[placement] = hold
 			placement++
 		}
+	}
+}
+
+func moveZeroes2(nums []int) {
+	i, j := 0, 1
+	for j < len(nums) {
+		if nums[i] != 0 {
+			i++
+		}
+		if nums[i] == 0 && nums[j] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+		}
+		j++
 	}
 }
